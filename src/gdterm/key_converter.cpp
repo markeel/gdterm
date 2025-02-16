633e359ed564ab768d636751600651c6121a71b5
markeel <markeel@users.noreply.github.com>
2025-02-16 09:17:12 -0700

    Fix support for strcpy_s
    The strcpy_s is apparently optional, even though on windows it
    says it is deprecated.  It isn't supported by gcc, so switching
    back to strcpy.  The build tools were not smart enough to detect
    this compile failure and stop, so the scripts are updated to
    properly detect a compiler error and propogate that out to the
    docker scripts.

--- src/gdterm/key_converter.cpp
+++ src/gdterm/key_converter.cpp
@@ -242,83 +242,83 @@ fill_term_string(char * buffer, int buf_len, wchar_t unicode, godot::Key key) {
 		case godot::Key::KEY_F35:
 			break;
 		case godot::Key::KEY_ESCAPE:
-			strcpy_s(buffer, buf_len, "\x1b");
+			strcpy(buffer, "\x1b");
 			break;
 		case godot::Key::KEY_TAB:
-			strcpy_s(buffer, buf_len, "\t");
+			strcpy(buffer, "\t");
 			break;
 		case godot::Key::KEY_BACKSPACE:
 			if (ctrl) {
-				strcpy_s(buffer, buf_len, TMT_KEY_BACKSPACE);
+				strcpy(buffer, TMT_KEY_BACKSPACE);
 			} else {
-				strcpy_s(buffer, buf_len, "\x7f");
+				strcpy(buffer, "\x7f");
 			}
 			break;
 		case godot::Key::KEY_DELETE:
-			strcpy_s(buffer, buf_len, "\x1b[3~");
+			strcpy(buffer, "\x1b[3~");
 			break;
 		case godot::Key::KEY_KP_ENTER:
-			strcpy_s(buffer, buf_len, "\r");
+			strcpy(buffer, "\r");
 			break;
 		case godot::Key::KEY_ENTER:
-			strcpy_s(buffer, buf_len, "\r");
+			strcpy(buffer, "\r");
 			break;
 		case godot::Key::KEY_INSERT:
-			strcpy_s(buffer, buf_len, TMT_KEY_INSERT);
+			strcpy(buffer, TMT_KEY_INSERT);
 			break;
 		case godot::Key::KEY_HOME:
-			strcpy_s(buffer, buf_len, TMT_KEY_HOME);
+			strcpy(buffer, TMT_KEY_HOME);
 			break;
 		case godot::Key::KEY_END:
-			strcpy_s(buffer, buf_len, TMT_KEY_END);
+			strcpy(buffer, TMT_KEY_END);
 			break;
 		case godot::Key::KEY_LEFT:
-			strcpy_s(buffer, buf_len, TMT_KEY_LEFT);
+			strcpy(buffer, TMT_KEY_LEFT);
 			break;
 		case godot::Key::KEY_UP:
-			strcpy_s(buffer, buf_len, TMT_KEY_UP);
+			strcpy(buffer, TMT_KEY_UP);
 			break;
 		case godot::Key::KEY_RIGHT:
-			strcpy_s(buffer, buf_len, TMT_KEY_RIGHT);
+			strcpy(buffer, TMT_KEY_RIGHT);
 			break;
 		case godot::Key::KEY_DOWN:
-			strcpy_s(buffer, buf_len, TMT_KEY_DOWN);
+			strcpy(buffer, TMT_KEY_DOWN);
 			break;
 		case godot::Key::KEY_PAGEUP:
-			strcpy_s(buffer, buf_len, TMT_KEY_PAGE_UP);
+			strcpy(buffer, TMT_KEY_PAGE_UP);
 			break;
 		case godot::Key::KEY_PAGEDOWN:
-			strcpy_s(buffer, buf_len, TMT_KEY_PAGE_DOWN);
+			strcpy(buffer, TMT_KEY_PAGE_DOWN);
 			break;
 		case godot::Key::KEY_F1:
-			strcpy_s(buffer, buf_len, TMT_KEY_F1);
+			strcpy(buffer, TMT_KEY_F1);
 			break;
 		case godot::Key::KEY_F2:
-			strcpy_s(buffer, buf_len, TMT_KEY_F2);
+			strcpy(buffer, TMT_KEY_F2);
 			break;
 		case godot::Key::KEY_F3:
-			strcpy_s(buffer, buf_len, TMT_KEY_F3);
+			strcpy(buffer, TMT_KEY_F3);
 			break;
 		case godot::Key::KEY_F4:
-			strcpy_s(buffer, buf_len, TMT_KEY_F4);
+			strcpy(buffer, TMT_KEY_F4);
 			break;
 		case godot::Key::KEY_F5:
-			strcpy_s(buffer, buf_len, TMT_KEY_F5);
+			strcpy(buffer, TMT_KEY_F5);
 			break;
 		case godot::Key::KEY_F6:
-			strcpy_s(buffer, buf_len, TMT_KEY_F6);
+			strcpy(buffer, TMT_KEY_F6);
 			break;
 		case godot::Key::KEY_F7:
-			strcpy_s(buffer, buf_len, TMT_KEY_F7);
+			strcpy(buffer, TMT_KEY_F7);
 			break;
 		case godot::Key::KEY_F8:
-			strcpy_s(buffer, buf_len, TMT_KEY_F8);
+			strcpy(buffer, TMT_KEY_F8);
 			break;
 		case godot::Key::KEY_F9:
-			strcpy_s(buffer, buf_len, TMT_KEY_F9);
+			strcpy(buffer, TMT_KEY_F9);
 			break;
 		case godot::Key::KEY_F10:
-			strcpy_s(buffer, buf_len, TMT_KEY_F10);
+			strcpy(buffer, TMT_KEY_F10);
 			break;
 		default:
 		{
@@ -337,101 +337,3 @@ fill_term_string(char * buffer, int buf_len, wchar_t unicode, godot::Key key) {
 	}
 }
 
-/*
-const char * 
-lookup_term_string(godot::Key key) {
-	const char * return_val = "";
-	int code = key & godot::KeyModifierMask::KEY_CODE_MASK; 
-	if (code == godot::Key::KEY_TAB) {
-		printf("Tab key\n");
-	} else {
-		printf("NOT Tab key\n");
-	}
-	int modifiers = key & godot::KeyModifierMask::KEY_MODIFIER_MASK; 
-	if ((code >= godot::Key::KEY_AT) && (code <= godot::Key::KEY_UNDERSCORE)) {
-		return_val = lower_case[code-godot::Key::KEY_AT];
-		if (modifiers & godot::KeyModifierMask::KEY_MASK_CTRL) {
-			return_val = control_code[code-godot::Key::KEY_AT];
-		}
-		if ((code >= godot::Key::KEY_A) && (code <= godot::Key::KEY_Z)) {
-			if (modifiers & godot::KeyModifierMask::KEY_MASK_SHIFT) {
-				return_val = upper_case[code-godot::Key::KEY_A];
-			}
-		}
-	} else if ((code >= godot::Key::KEY_KP_0) && (code <= godot::Key::KEY_KP_9)) {
-		return_val = numbers[code-godot::Key::KEY_KP_0];
-	} else if ((code >= godot::Key::KEY_SPACE) && (code <= godot::Key::KEY_SLASH)) {
-		return_val = symbols1[code-godot::Key::KEY_SPACE];
-	} else if ((code >= godot::Key::KEY_0) && (code <= godot::Key::KEY_9)) {
-		return_val = numbers[code-godot::Key::KEY_0];
-	} else if ((code >= godot::Key::KEY_COLON) && (code <= godot::Key::KEY_QUESTION)) {
-		return_val = symbols2[code-godot::Key::KEY_COLON];
-	} else if ((code >= godot::Key::KEY_BRACKETLEFT) && (code <= godot::Key::KEY_QUOTELEFT)) {
-		return_val = symbols3[code-godot::Key::KEY_BRACKETLEFT];
-	} else if ((code >= godot::Key::KEY_BRACELEFT) && (code <= godot::Key::KEY_ASCIITILDE)) {
-		return_val = symbols4[code-godot::Key::KEY_BRACELEFT];
-	} else {
-		switch (code) {
-			case godot::Key::KEY_ESCAPE:
-				return "\x1b\x1b";
-			case godot::Key::KEY_TAB:
-				return "\t";
-			case godot::Key::KEY_BACKSPACE:
-				return TMT_KEY_BACKSPACE;
-			case godot::Key::KEY_ENTER:
-			case godot::Key::KEY_KP_ENTER:
-				return "\r";
-			case godot::Key::KEY_INSERT:
-				return TMT_KEY_INSERT;
-			case godot::Key::KEY_DELETE:
-				return "\x1f";
-			case godot::Key::KEY_HOME:
-				return TMT_KEY_HOME;
-			case godot::Key::KEY_END:
-				return TMT_KEY_END;
-			case godot::Key::KEY_LEFT:
-				return TMT_KEY_LEFT;
-			case godot::Key::KEY_UP:
-				return TMT_KEY_UP;
-			case godot::Key::KEY_RIGHT:
-				return TMT_KEY_RIGHT;
-			case godot::Key::KEY_DOWN:
-				return TMT_KEY_DOWN;
-			case godot::Key::KEY_PAGEUP:
-				return TMT_KEY_PAGE_UP;
-			case godot::Key::KEY_PAGEDOWN:
-				return TMT_KEY_PAGE_DOWN;
-			case godot::Key::KEY_F1:
-				return TMT_KEY_F1;
-			case godot::Key::KEY_F2:
-				return TMT_KEY_F2;
-			case godot::Key::KEY_F3:
-				return TMT_KEY_F3;
-			case godot::Key::KEY_F4:
-				return TMT_KEY_F4;
-			case godot::Key::KEY_F5:
-				return TMT_KEY_F5;
-			case godot::Key::KEY_F6:
-				return TMT_KEY_F6;
-			case godot::Key::KEY_F7:
-				return TMT_KEY_F7;
-			case godot::Key::KEY_F8:
-				return TMT_KEY_F8;
-			case godot::Key::KEY_F9:
-				return TMT_KEY_F9;
-			case godot::Key::KEY_F10:
-				return TMT_KEY_F10;
-			case godot::Key::KEY_KP_MULTIPLY:
-				return "*";
-			case godot::Key::KEY_KP_DIVIDE:
-				return "/";
-			case godot::Key::KEY_KP_SUBTRACT:
-				return "-";
-			case godot::Key::KEY_KP_PERIOD:
-				return ".";
-		}
-	}
-	
-	return return_val;
-}
-*/
